b2ab89a6297a1cc9d0528e8ebb2f984966dae3b4
Varsha Wijethunge <varrave16@gmail.com>
2023-07-31 18:06:06 +0530

    Add User Insertion Done

--- client/src/components/PopupComponent.jsx
+++ client/src/components/PopupComponent.jsx
@@ -5,11 +5,47 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Grid from '@mui/material/Grid';
 import SideImageComponent from "./SideImageComponent";
+import axios from 'axios';
 
 import { Button, Box, Dialog, DialogActions, DialogContent, IconButton, Stack, TextField, Typography } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close"
 
+
 const Popup = () => { // Changed the function name to start with uppercase "P"
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    role: '',
+    contact: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+  
+    // For the "role" field, we need to handle its value differently
+    if (name === 'role') {
+      // Access the selected value using event.target.value directly
+      setFormData({ ...formData, [name]: e.target.value });
+    } else {
+      // For other fields, use the standard way
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:8080/api/users/addUsers', formData)
+      .then((response) => {
+        console.log('User created:', response.data);
+        // Handle success, display success message or redirect if needed
+      })
+      .catch((error) => {
+        console.error('Error creating user:', error);
+        // Handle error, display error message
+      });
+    }
     const [open, setOpen] = useState(false); // Changed the state variable name to setOpen
     const functionOpenPopup = () => { // Changed the function name to start with "use"
         setOpen(true); // Changed the function to use setOpen
@@ -17,10 +53,17 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
     const closePopup = () => { // Changed the function name to start with "use"
         setOpen(false); // Changed the function to use setOpen
     }
-    const [role, setRole] = React.useState('');
-  const handleChange = (event) => {
-    setRole(event.target.value);
-  };
+    const handleReset = () => {
+      setFormData({
+        firstName: '',
+        lastName: '',
+      email: '',
+      password: '',
+      role: '',
+      contact: '',
+      });
+    };
+
     return (
         <div style={{textAlign:'center'}}>
       
@@ -29,6 +72,7 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
             <Dialog 
             // fullScreen 
             open={open} onClose={closePopup} fullWidth maxWidth="md" >
+              <form onSubmit={handleSubmit}>
             <Grid container direction="row" >
               <Grid container xs={4} sx={{backgroundColor:"#1A73E8"}}>
                 <SideImageComponent />
@@ -51,12 +95,16 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
                     {/* <DialogContentText>Do you want to remove this user?</DialogContentText> */}
                     <Stack spacing={2} margin={2}>
                     <Grid container spacing={2}>
+                    
               <Grid item xs={12} sm={6}>
+              
                 <TextField
                   autoComplete="given-name"
                   name="firstName"
                   required
                   fullWidth
+                  value={formData.firstName}
+                  onChange={handleChange}
                   id="firstName"
                   label="First Name"
                   autoFocus
@@ -69,6 +117,8 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
                   id="lastName"
                   label="Last Name"
                   name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
                   autoComplete="family-name"
                 />
               </Grid>
@@ -79,6 +129,8 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
                   id="email"
                   label="Email Address"
                   name="email"
+                  value={formData.email}
+                  onChange={handleChange}
                   autoComplete="email"
                 />
               </Grid>
@@ -90,10 +142,32 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
                   label="Password"
                   type="password"
                   id="password"
+                  value={formData.password}
+                  onChange={handleChange}
                   autoComplete="new-password"
                 />
-              
               </Grid>
+
+              <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+              
+                <InputLabel id="demo-simple-select-label" required>Role</InputLabel>
+                <Select
+                  required
+                  label="Role"
+                  id="role"
+                  value={formData.role}
+                  onChange={handleChange}
+                  name="role"
+                >
+                    <MenuItem value={'lecturer'}>UCSC Lecturer</MenuItem>
+                    <MenuItem value={'visitinglecturer'}>Visiting Lecturer</MenuItem>
+                    <MenuItem value={'Technical'}>Technical Assistant</MenuItem>
+                    <MenuItem value={'staff'}>Staff</MenuItem>
+                </Select>
+                </FormControl>
+              </Grid>
+
               <Grid item xs={12}>
                 <TextField
                   required
@@ -102,25 +176,10 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
                   label="Contact No"
                   type="phone"
                   id="contact"
+                  value={formData.contact}
+                  onChange={handleChange}
                   autoComplete="contact"
                 />
-              </Grid>
-              <Grid item xs={12}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Role</InputLabel>
-                <Select
-                  required
-                  label="Role"
-                  id="role"
-                  value={role}
-                  onChange={handleChange}
-                >
-                    <MenuItem value={'lecturer'}>UCSC Lecturer</MenuItem>
-                    <MenuItem value={'visitinglecturer'}>Visiting Lecturer</MenuItem>
-                    <MenuItem value={'Technical'}>Technical Assistant</MenuItem>
-                    <MenuItem value={'staff'}>Staff</MenuItem>
-                </Select>
-                </FormControl>
               </Grid>
             </Grid>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -132,13 +191,14 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
                                     Add
                                 </Button>
                                 <Button
-                                    type='reset'
+                                    type="reset"
                                     variant="contained"
-                                    
+                                    onClick={handleReset}
                                     sx={{ mt: 3, minWidth: 150, minHeight: 30 ,justifyItems: 'left', fontSize:18, backgroundColor: '#7B809A', ":hover":{background:'#495361'}}}
                                 >
-                                    Cancel
+                                    Reset
                                 </Button>
+                                
                             </div>
                     </Stack>
                 </DialogContent>
@@ -148,9 +208,9 @@ const Popup = () => { // Changed the function name to start with uppercase "P"
                 </DialogActions>
                 </Grid>
               </Grid>
+              </form>
             </Dialog>
         </div>
     );
-}
-
+  }
 export default Popup; // Changed the export name to match the component name
--- server/src/main/java/com/postgresql/pgms/repo/UsersRepo.java
+++ server/src/main/java/com/postgresql/pgms/repo/UsersRepo.java
@@ -1,9 +1,9 @@
 package com.postgresql.pgms.repo;
 
-import com.postgresql.pgms.model.lecturer;
+import com.postgresql.pgms.model.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface LecturerRepo extends JpaRepository<lecturer, Long> {
+public interface UsersRepo extends JpaRepository<users, Long> {
 }

90e2532e6755c354df3e8a23dbbea613c26936ef
Dcode202 <49726802+Dcode202@users.noreply.github.com>
2023-08-02 19:00:14 +0530

    update on table

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router';
 import Dashboard from './layouts/dashboard';
 import Users from './layouts/users';
+import Demo from './layouts/demo';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="/users" element={<Users/>} />
+          <Route path="/demo" element={<Demo/>} />
         </Routes>
       </div>
     </Router>
--- client/src/components/GeneralTable.jsx
+++ client/src/components/GeneralTable.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import user from '../assets/images/user.png';
+import TablePagination from '@mui/material/TablePagination';
 
 
 const GeneralTable = ({ columns, data }) => {
@@ -29,6 +30,19 @@ const GeneralTable = ({ columns, data }) => {
     const isNameColumn = (columnName) => {
         return columnName === 'NAME';
     };
+
+    const [rowsPerPage, setRowsPerPage] = useState(5); // Number of rows per page
+    const [page, setPage] = useState(0); // Current page number
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0); // Reset the page number when rows per page changes
+    };
+    const startRowIndex = page * rowsPerPage;
+    const endRowIndex = startRowIndex + rowsPerPage;
+    const visibleRows = data.slice(startRowIndex, endRowIndex);
 
     return (
         <><div style={{
@@ -60,14 +74,14 @@ const GeneralTable = ({ columns, data }) => {
                 }
             }} />
         </div>
-        
-        <TableContainer component={Paper} sx={{
-            backgroundColor: '#FFFFFF',
-            position: 'relative',
-            top: -40,
-            zIndex: 0,
-            paddingTop: 8,
-        }}>
+
+            <TableContainer component={Paper} sx={{
+                backgroundColor: '#FFFFFF',
+                position: 'relative',
+                top: -40,
+                zIndex: 0,
+                paddingTop: 8,
+            }}>
                 <ToggleButtonGroup
                     value={alignment}
                     exclusive
@@ -83,32 +97,28 @@ const GeneralTable = ({ columns, data }) => {
                         marginRight: 2.5,
                     }}
                 >
-                    <ToggleButton value="Online" sx={{ 
-                        border: 'none', 
+                    <ToggleButton value="Online" sx={{
+                        border: 'none',
                         fontSize: 10,
                         color: '#343A40',
                         fontFamily: 'Inter',
                         borderRight: '1px solid rgba(0, 0, 0, 0.1)',
                         transition: '0.3s',
-                        }}>
-                            Online
+                    }}>
+                        Online
                     </ToggleButton>
-                    <ToggleButton value="Offline" sx={{ 
-                        border: 'none', 
+                    <ToggleButton value="Offline" sx={{
+                        border: 'none',
                         fontSize: 10,
                         color: '#343A40',
                         fontFamily: 'Inter',
                         transition: '0.3s',
-                        }}>
-                            Offline
+                    }}>
+                        Offline
                     </ToggleButton>
                 </ToggleButtonGroup>
 
-                <Table sx={{
-                    '# .MuiTable-roots': {
-                        
-                    }
-                }}>
+                <Table>
                     <TableHead>
                         <TableRow>
                             {columns.map((column) => (
@@ -131,22 +141,22 @@ const GeneralTable = ({ columns, data }) => {
                             <TableRow key={rowIndex}>
                                 {columns.map((column, colIndex) => (
                                     <TableCell key={`${rowIndex}-${colIndex}`}
-                                    sx={{
-                                        paddingLeft: 3,
-                                        color: '#7B809A',
-                                        fontFamily: 'Roboto',
-                                        fontSize: '12px',
-                                        fontWeight: '700',
-                                        paddingTop: 2.3,
-                                        paddingBottom: 2.3,
-                                    }}>
+                                        sx={{
+                                            paddingLeft: 3,
+                                            color: '#7B809A',
+                                            fontFamily: 'Roboto',
+                                            fontSize: '12px',
+                                            fontWeight: '700',
+                                            paddingTop: 2.3,
+                                            paddingBottom: 2.3,
+                                        }}>
                                         {isOnline(column, row[column]) ? (
-                                            <div style={{ 
+                                            <div style={{
                                                 background: 'linear-gradient(180deg, #66BB6A 0%, #43A047 100%)',
                                                 borderRadius: 50,
                                                 color: '#FFF',
                                                 width: 80,
-                                                fontFamily : 'Roboto',
+                                                fontFamily: 'Roboto',
                                                 fontSize: '12px',
                                                 fontWeight: '900',
                                                 display: 'flex',
@@ -155,56 +165,56 @@ const GeneralTable = ({ columns, data }) => {
                                                 paddingTop: 2,
                                             }}>{row[column]}</div>
                                         ) : isOffline(column, row[column]) ? (
-                                                <div style={{
-                                                    background: 'black',
-                                                    borderRadius: 50,
-                                                    color: '#FFF',
-                                                    width: 80,
-                                                    fontFamily: 'Roboto',
-                                                    fontSize: '12px',
-                                                    fontWeight: '900',
-                                                    display: 'flex',
-                                                    justifyContent: 'center',
-                                                    alignItems: 'center',
-                                                    paddingTop: 2,
-                                                }}>{row[column]}</div>
+                                            <div style={{
+                                                background: 'black',
+                                                borderRadius: 50,
+                                                color: '#FFF',
+                                                width: 80,
+                                                fontFamily: 'Roboto',
+                                                fontSize: '12px',
+                                                fontWeight: '900',
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                paddingTop: 2,
+                                            }}>{row[column]}</div>
                                         ) : isFunctionColumn(column) ? (
                                             <div>
                                                 <span>{row[column]}</span><br />
-                                                    <span style={{
-                                                        color: '#555', 
-                                                        fontSize: '9px', 
-                                                        fontWeight: '400'
-                                                    }}>UCSC</span>
+                                                <span style={{
+                                                    color: '#555',
+                                                    fontSize: '9px',
+                                                    fontWeight: '400'
+                                                }}>UCSC</span>
                                             </div>
-                                            ) : isNameColumn(column) ? (
+                                        ) : isNameColumn(column) ? (
+                                            <div style={{
+                                                display: 'flex'
+                                            }}>
                                                 <div style={{
-                                                    display: 'flex'
+                                                    marginRight: 10,
+                                                    display: 'flex',
+                                                    alignItems: 'center'
                                                 }}>
-                                                    <div style={{
-                                                        marginRight: 10,
-                                                        display: 'flex',
-                                                        alignItems: 'center'
-                                                        }}>
-                                                        <img src={user} width={30} height={30} alt="user"/>
-                                                    </div>
-                                                    <div>
-                                                        <span style={{
-                                                            color: '#000',
-                                                            fontSize: '14px',
-                                                            fontWeight: '500'
-                                                        }}>{row[column]}</span>
-                                                        <br />
-                                                        <span style={{
-                                                            color: '#4A4949',
-                                                            fontSize: '10px',
-                                                            fontWeight: '400'
-                                                            }}>john@gmail.com</span>
-                                                    </div>
+                                                    <img src={user} width={30} height={30} alt="user" />
                                                 </div>
-                                            ) : (
-                                                row[column]
-                                            )
+                                                <div>
+                                                    <span style={{
+                                                        color: '#000',
+                                                        fontSize: '14px',
+                                                        fontWeight: '500'
+                                                    }}>{row[column]}</span>
+                                                    <br />
+                                                    <span style={{
+                                                        color: '#4A4949',
+                                                        fontSize: '10px',
+                                                        fontWeight: '400'
+                                                    }}>john@gmail.com</span>
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            row[column]
+                                        )
                                         }
                                     </TableCell>
                                 ))}
@@ -212,7 +222,16 @@ const GeneralTable = ({ columns, data }) => {
                         ))}
                     </TableBody>
                 </Table>
-            </TableContainer></>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={data.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            /></>
     );
 };
 

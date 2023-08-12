642c2e3b081bec5f92c3b74465843841e4dd6788
Mudheera Kariyawasam <2020is055@stu.ucsc.cmb.ac.lk>
2023-08-12 16:15:41 +0530

    course controllers done, basic program controllers added

---
+++ server/src/main/java/com/postgresql/pgms/Service/programServiceIMPL.java
@@ -0,0 +1,19 @@
+package com.postgresql.pgms.Service;
+
+import com.postgresql.pgms.DTO.ProgramDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class programServiceIMPL implements programService{
+    @Override
+    public String saveProgram(ProgramDTO programDTO) {
+        return null;
+    }
+
+    @Override
+    public List<ProgramDTO> getAllPrograms() {
+        return null;
+    }
+}

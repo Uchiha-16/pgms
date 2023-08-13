ce5bb4cbd9c89cd05b8452adeb95c6b90c3f82f4
Dcode202 <49726802+Dcode202@users.noreply.github.com>
2023-08-13 20:50:26 +0530

    Update login.jsx

--- client/src/layouts/login.jsx
+++ client/src/layouts/login.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Box, Container, Paper, Typography, TextField, Button } from '@mui/material';
+import styled from "styled-components";
+import { keyframes } from "styled-components";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import backgroundImg from '../assets/images/login_background.jpg'; // Replace with the path to your background image
 import logoImg from '../assets/images/logo.png'; // Replace with the path to your logo image
+import { color } from '@mui/system';
+import FooterComponent from '../components/FooterComponent';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const theme = createTheme({
   typography: {
@@ -10,40 +15,76 @@ const theme = createTheme({
   },
 });
 
+const gradient = keyframes`
+  0% {
+    background-position: 0 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
+  }
+`;
+
+const AnimatedGradientBackground = styled.div`
+  animation: ${gradient} 15s ease-in-out infinite;
+  background: linear-gradient(-45deg, #49A3F1, #1A73E8, #EC407A, #5B86E5);
+  background-size: 300%;
+  height: 100vh; /* Adjust the height as needed */
+  width: 100%; /* Full width */
+`;
+
+const Boxbg = styled.div`
+  /* Your other Box styles */
+`;
+
+
 const LoginPage = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Box
+      <AnimatedGradientBackground><Boxbg
         sx={{
-          backgroundImage: `url(${backgroundImg})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          minHeight: '100vh',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          top: '11rem',
+          position: 'relative',
         }}
       >
-        <Container maxWidth="xs" sx={{ height: '375px', width: '30%' }}>
+        <Container sx={{
+          height: '100vh', 
+          display: 'flex', 
+          justifyContent: 'center',
+          alignItems: 'center',
+          }}>
           <Paper
             sx={{
-              padding: 4,
               textAlign: 'center',
-              backgroundColor: `rgba(0, 0, 0, ${0.8})`, // Set the background color with transparency
-              boxShadow: '0px 0px 8px #7B809A', // Silver outline
-              height: '100%',
+              backgroundColor: 'white', // Set the background color with transparency
+              boxShadow: '0px 13px 20px -7px rgba(0, 0, 0, 0.2);', 
+              height: '375px',
+              width: '18rem',
+              paddingLeft: '10rem',
+              paddingRight: '10rem',
+              paddingBottom: '3rem',
+              paddingTop: '2rem',
               display: 'flex',
               flexDirection: 'column',
               justifyContent: 'center',
+              borderRadius: '20px', // Rounded corners
             }}
           >
-            <Box sx={{ position: 'relative', marginBottom: 2 }}>
+            <Box sx={{ 
+              position: 'relative', 
+              marginBottom: 2,
+              }}>
               <img
                 src={logoImg}
                 alt="Logo"
                 style={{
-                  width: '80px', // Adjust the size of the logo as needed
-                  height: '80px', // Adjust the size of the logo as needed
+                  width: '85px', // Adjust the size of the logo as needed
+                  height: '85px', // Adjust the size of the logo as needed
                   borderRadius: '50%',
                   position: 'absolute',
                   top: '-100px', // Move the logo higher up by increasing the negative value
@@ -51,10 +92,12 @@ const LoginPage = () => {
                 }}
               />
             </Box>
-            <Typography color={'white'}>
-              <div>Postgraduate Management System</div>
-              <br />
-              
+            <Typography color={'#011632'} sx={{
+              fontWeight: '600', 
+              marginBottom: '1rem',
+            }}>
+              Postgraduate Management System
+              <br /><br />
               WELCOME
             </Typography>
             <TextField
@@ -64,15 +107,24 @@ const LoginPage = () => {
               variant="outlined"
               sx={{
                 '& .MuiOutlinedInput-notchedOutline': {
-                  borderColor: '#7B809A', // Silver outline for text field
+                  borderColor: '#F0F2F5', // Silver outline for text field
+                  transition: 'border-color 0.2s ease-in-out', // Add a transition on hover
+                  borderRadius: '10px', // Rounded corners
+                },
+                ":hover": {
+                  '& .MuiOutlinedInput-notchedOutline': {
+                    borderColor: '#1A73E8',
+                  },
                 },
                 '& input': {
                   height: '15px', // Adjust the height as needed
                   width: '100%',
-                  color: '#7B809A', // Adjust the width as needed
+                  color: '#011632', // Adjust the width as needed
+                  fontSize: '14px',
                 },
                 '& .MuiInputLabel-root': {
-                  color: '#7B809A', // Set the color of the label
+                  color: '#898989', // Set the color of the label
+                  fontSize: '14px',
                 },
                 fontFamily: 'Inter, sans-serif', // Set the font family to "Inter"
               }}
@@ -84,15 +136,24 @@ const LoginPage = () => {
               variant="outlined"
               sx={{
                 '& .MuiOutlinedInput-notchedOutline': {
-                  borderColor: '#7B809A', // Silver outline for text field
+                  borderColor: '#F0F2F5', // Silver outline for text field
+                  transition: 'border-color 0.2s ease-in-out', // Add a transition on hover
+                  borderRadius: '10px', // Rounded corners
+                },
+                ":hover": {
+                  '& .MuiOutlinedInput-notchedOutline': {
+                    borderColor: '#1A73E8', // Silver outline for text field
+                  },
                 },
                 '& input': {
                   height: '15px', // Adjust the height as needed
                   width: '100%',
-                  color: '#7B809A', // Adjust the width as needed
+                  color: '#011632', // Adjust the width as needed
+                  fontSize: '14px',
                 },
                 '& .MuiInputLabel-root': {
-                  color: '#7B809A', // Set the color of the label
+                  color: '#898989', // Set the color of the label
+                  fontSize: '14px',
                 },
                 fontFamily: 'Inter, sans-serif', // Set the font family to "Inter"
               }}
@@ -107,16 +168,25 @@ const LoginPage = () => {
                 textTransform: 'none', // Disable capitalization of the button text
                 color: '#1A73E8', // Fill color for the button text
                 fontFamily: 'Inter, sans-serif', // Set the font family to "Inter"
+                '& .MuiTouchRipple-root': {
+                  display: 'none', // Disable the ripple effect
+                },
+                "&:hover": {
+                  //you want this to be the same as the backgroundColor above
+                  backgroundColor: "transparent",
+                  color: '#E53935'
+                },
+                fontSize: '12px',
               }}
             >
-              Forgot Password
-            </Button>
+              Forgot Password?
+            </Button><br />
             <Button variant="contained" color="primary" fullWidth sx={{ backgroundColor: '#2C85EB', fontFamily: 'Inter, sans-serif', fontSize: '15px', fontWeight: 'regular' }}>
               login
             </Button>
           </Paper>
         </Container>
-      </Box>
+      </Boxbg></AnimatedGradientBackground>
     </ThemeProvider>
   );
 };

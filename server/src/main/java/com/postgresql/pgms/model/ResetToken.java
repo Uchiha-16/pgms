6221486dd56ecc09565594c908f3bffb0f306554
MSWSal <sahansalgado10@gmail.com>
2023-08-15 03:34:41 +0530

    Reset Model

---
+++ server/src/main/java/com/postgresql/pgms/model/ResetToken.java
@@ -0,0 +1,29 @@
+package com.postgresql.pgms.model;
+
+import com.postgresql.pgms.enumeration.TokenType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "reset-token")
+public class ResetToken {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType = TokenType.RESET_PASSWORD;
+
+    private boolean expired;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id" )
+    private Users user;
+}

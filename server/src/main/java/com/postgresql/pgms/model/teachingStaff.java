4637716da6a89d9bfd248f6762fe047d4eeb616e
MSWSal <sahansalgado10@gmail.com>
2023-09-15 18:52:10 +0530

    @s for teachingStaff.java

--- server/src/main/java/com/postgresql/pgms/model/teachingStaff.java
+++ server/src/main/java/com/postgresql/pgms/model/teachingStaff.java
@@ -1,9 +1,15 @@
 package com.postgresql.pgms.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table (name = "TeachingStaff")
 public class teachingStaff {

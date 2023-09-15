d7e34c5367d9cd8a898a2b6ee08fcf64d228addc
MSWSal <sahansalgado10@gmail.com>
2023-09-15 18:43:43 +0530

    @s for applyCourse.java

--- server/src/main/java/com/postgresql/pgms/model/applyCourse.java
+++ server/src/main/java/com/postgresql/pgms/model/applyCourse.java
@@ -3,9 +3,15 @@ package com.postgresql.pgms.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table (name = "ApplyCourse")
 public class applyCourse {

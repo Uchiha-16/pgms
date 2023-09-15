89982b5561af2da96b79125dfa600448acedea9d
MSWSal <sahansalgado10@gmail.com>
2023-09-15 18:49:56 +0530

    @s for programCordinator.java

--- server/src/main/java/com/postgresql/pgms/model/programCordinator.java
+++ server/src/main/java/com/postgresql/pgms/model/programCordinator.java
@@ -1,9 +1,15 @@
 package com.postgresql.pgms.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table (name = "ProgramCordinator")
 public class programCordinator {

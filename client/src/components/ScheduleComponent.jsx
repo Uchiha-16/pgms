6dfca04fdf7c712ea237ddd312fdd1bb2210a2b4
Dcode202 <49726802+Dcode202@users.noreply.github.com>
2023-10-31 20:37:58 +0530

    Update ScheduleComponent.jsx

--- client/src/components/ScheduleComponent.jsx
+++ client/src/components/ScheduleComponent.jsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
-import { 
-    Paper, 
-    Box, 
+import { styled } from '@mui/material/styles';
+import {
+    Paper,
+    Box,
     TableContainer,
     ToggleButton,
-    ToggleButtonGroup, 
+    ToggleButtonGroup,
+    Checkbox,
 } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { green, lightBlue } from '@mui/material/colors';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import {
     ViewState, EditingState, GroupingState, IntegratedGrouping, IntegratedEditing,
 } from '@devexpress/dx-react-scheduler';
@@ -24,6 +27,40 @@ import {
 import { data as appointments } from '../config/timetableData';
 
 const isWeekOrMonthView = viewName => viewName === 'Week';
+
+
+const PREFIX = 'ScheduleComponent';
+// #FOLD_BLOCK
+const classes = {
+    formControlLabel: `${PREFIX}-formControlLabel`,
+    text: `${PREFIX}-text`,
+};
+// #FOLD_BLOCK
+const StyledFormControlLabel = styled(FormControlLabel)(({
+    theme: { spacing, palette, typography },
+}) => ({
+    [`&.${classes.formControlLabel}`]: {
+        padding: spacing(2),
+        paddingLeft: spacing(10),
+    },
+    [`&.${classes.text}`]: {
+        ...typography.caption,
+        color: palette.text.secondary,
+        fontWeight: 'bold',
+        fontSize: '1rem',
+    },
+}));
+
+const GroupOrderSwitcher = (({ isGroupByDate, onChange }) => (
+    <StyledFormControlLabel
+        control={
+            <Checkbox checked={isGroupByDate} onChange={onChange} color="primary" style={{display:'none'}}/>
+        }
+        className={classes.formControlLabel}
+        classes={{ label: classes.text }}
+    />
+));
+
 
 
 // Get the current date
@@ -47,7 +84,7 @@ const sundayDate = sunday.getDate();
 console.log("Saturday:", saturdayDate);
 console.log("Sunday:", sundayDate);
 
-export default class Schedule extends React.PureComponent {
+export default class ScheduleComponent extends React.PureComponent {
     constructor(props) {
         super(props);
 
@@ -68,16 +105,18 @@ export default class Schedule extends React.PureComponent {
                     { text: 'MBA', id: 'MBA', color: '#dda0dd' },
                     { text: 'MIS', id: 'MIS', color: '#FFB939' },
                 ],
-            }, {
-                fieldName: 'semester',
-                title: 'Semester',
-                instances: [
-                    { text: 'Semester 4', id: 1 },
-                    { text: 'Semester 2', id: 2 },
-                ],
-            }],
+            },
+                {
+                    fieldName: 'semester',
+                    title: 'Semester',
+                    instances: [
+                        { text: 'Semester 4', id: 1 },
+                        { text: 'Semester 2', id: 2 },
+                    ],
+                }
+            ],
             grouping: [
-                { resourceName: 'semester' },
+                // { resourceName: 'semester' },
                 { resourceName: 'priorityId' },
             ],
             groupByDate: isWeekOrMonthView,
@@ -87,19 +126,32 @@ export default class Schedule extends React.PureComponent {
 
         // Bind event handlers
         this.commitChanges = this.commitChanges.bind(this);
+        this.onGroupOrderChange = () => {
+            const { isGroupByDate } = this.state;
+            this.setState({
+                isGroupByDate: !isGroupByDate,
+                groupByDate: isGroupByDate ? undefined : isWeekOrMonthView,
+            });
+        };
         this.toggleSelectedDay = this.toggleSelectedDay.bind(this);
-        
+
     }
 
     // Add a method to toggle the selected day
     toggleSelectedDay(day) {
-        this.setState({ selectedDay: day });
+        let firstDayOfWeek = 6; // Saturday
+        if (day === 'Sunday') {
+            // Calculate the upcoming Sunday
+            const currentDayOfWeek = currentDate.getDay();
+            firstDayOfWeek = currentDayOfWeek === 0 ? 7 : 7 - currentDayOfWeek;
+        }
+
+        this.setState({ selectedDay: day, firstDayOfWeek });
     }
 
     commitChanges({ added, changed, deleted }) {
         this.setState((state) => {
             let { data } = state;
-            
             if (added) {
                 const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
                 data = [...data, { id: startingAddedId, ...added }];
@@ -117,142 +169,150 @@ export default class Schedule extends React.PureComponent {
 
     render() {
         const {
-            data, resources, grouping, groupByDate, selectedDay,
+            data, resources, grouping, groupByDate, selectedDay, isGroupByDate,
         } = this.state;
 
         // Define a variable to store the excluded days based on the selectedDay
         let excludedDays = [];
-        let sundaySelected = false;
+        let saturdaySelected = false;
         if (selectedDay === 'Saturday') {
             excludedDays = [0, 1, 2, 3, 4, 5]; // Exclude all days except Saturday
+            saturdaySelected = true;
         } else if (selectedDay === 'Sunday') {
-            excludedDays = [0, 1, 2, 4, 5, 6]; // Exclude all days except Sunday
-            sundaySelected = true;
+            // Calculate the following Sunday
+            excludedDays = [1, 2, 3, 4, 5, 6];
+            saturdaySelected = false;
         }
 
+
         return (
-                <Box>
-                    <Paper
+            <Box>
+                <GroupOrderSwitcher isGroupByDate={isGroupByDate} onChange={this.onGroupOrderChange} />
+                <Paper
+                    sx={{
+                        background: "transparent",
+                        boxShadow: "none",
+                    }}
+                >
+                    <TableContainer
                         sx={{
-                            background: "transparent",
-                            boxShadow: "none",
+                            backgroundColor: "#FFFFFF",
+                            position: "relative",
+                            top: "-4rem",
+                            paddingTop: "78px",
+                            paddingBottom: 3,
+                            borderRadius: "5px",
+                            "& .MainLayout-stickyElement": {
+                                borderTop: '1px solid #C9D1D8',
+                            }
                         }}
                     >
-                        <TableContainer
-                            sx={{
-                                backgroundColor: "#FFFFFF",
-                                position: "relative",
-                                top: "-2rem",
-                                paddingTop: "78px",
-                                paddingBottom: 3,
-                                borderRadius: "5px",
-                                "& .MainLayout-stickyElement": {
-                                    borderTop: '1px solid #C9D1D8',
-                                }
+                        {/* Toggle button */}
+                        <div style={{
+                            flex: 1,
+                            float: 'right',
+                            marginRight: '1.5rem',
+                            marginBottom: '2rem',
+                        }}>
+                            <ToggleButtonGroup sx={{
+                                height: '10px',
                             }}
-                        >
-                            {/* Toggle button */}
-                            <div style={{ 
-                                flex: 1,
-                                float: 'right',
-                                marginRight: '1.5rem',
-                                marginBottom: '2rem',
-                                }}>
-                                <ToggleButtonGroup sx={{
-                                    height: '10px',
-                                }}
-                                
+
+                            >
+                                <ToggleButton
+                                    value="Saturday"
+                                    sx={{
+                                        border: '1px solid #C9D1D8',
+                                        fontSize: '10px',
+                                        borderRadius: '20px',
+                                        padding: '1rem',
+                                        backgroundColor: selectedDay === 'Saturday' ? '#DEE2E6' : 'white',
+                                    }}
+                                    onClick={() => this.toggleSelectedDay('Saturday')}
                                 >
-                                    <ToggleButton
-                                        value="Saturday"
-                                        sx={{
-                                            border: '1px solid #C9D1D8',
-                                            fontSize: '10px',
-                                            borderRadius: '20px',
-                                            padding: '1rem',
-                                            backgroundColor: selectedDay === 'Saturday' ? '#DEE2E6' : 'white',
-                                        }}
-                                        onClick={() => this.toggleSelectedDay('Saturday')}
-                                    >
-                                        Sat
-                                    </ToggleButton>
-                                    <ToggleButton
-                                        value="Sunday"
-                                        sx={{
-                                            border: '1px solid #C9D1D8',
-                                            fontSize: '10px',
-                                            borderRadius: '20px',
-                                            padding: '1rem',
-                                            backgroundColor: selectedDay === 'Sunday' ? '#DEE2E6' : 'white',
-                                        }}
-                                        onClick={() => this.toggleSelectedDay('Sunday')}
-                                    >
-                                        Sun
-                                    </ToggleButton>
-                                    <ToggleButton
-                                        sx={{
-                                            border: '1px solid #C9D1D8',
-                                            fontSize: '9px',
-                                            borderRadius: '20px',
-                                            padding: '1rem',
-                                        }}
-                                    >
-                                        <AddCircleIcon fontSize="small" />
-                                    </ToggleButton>
-                                </ToggleButtonGroup>
-                            </div>
+                                    Sat
+                                </ToggleButton>
+                                <ToggleButton
+                                    value="Sunday"
+                                    sx={{
+                                        border: '1px solid #C9D1D8',
+                                        fontSize: '10px',
+                                        borderRadius: '20px',
+                                        padding: '1rem',
+                                        backgroundColor: selectedDay === 'Sunday' ? '#DEE2E6' : 'white',
+                                    }}
+                                    onClick={() => this.toggleSelectedDay('Sunday')}
+                                >
+                                    Sun
+                                </ToggleButton>
+                                <ToggleButton
+                                    sx={{
+                                        border: '1px solid #C9D1D8',
+                                        fontSize: '9px',
+                                        borderRadius: '20px',
+                                        padding: '1rem',
+                                    }}
+                                >
+                                    <AddCircleIcon fontSize="small" />
+                                </ToggleButton>
+                            </ToggleButtonGroup>
+                        </div>
 
-                                    <Scheduler
-                                        data={data}
-                                        height={'auto'}
-                                    >
-                                        <ViewState
-                                            defaultCurrentDate="2023-10-30"
-                                        />
-                                        <EditingState
-                                            onCommitChanges={this.commitChanges}
-                                        />
-                                        <GroupingState
-                                            grouping={grouping}
-                                            groupByDate={groupByDate}
-                                        />
+                        <Scheduler
+                            data={data}
+                            height={'auto'}
+                        >
+                            <ViewState
+                                defaultCurrentDate="2023-10-30"
+                            />
+                            <EditingState
+                                onCommitChanges={this.commitChanges}
+                            />
+                            <GroupingState
+                                grouping={grouping}
+                                groupByDate={groupByDate}
+                            />
 
-                                        <WeekView
-                                            startDayHour={8}
-                                            endDayHour={18}
-                                            excludedDays={excludedDays}
-                                            currentViewDate={sundaySelected ? sundayDate : saturdayDate}
-                                        />
 
-                                        <Appointments 
-                                            appointmentContentComponent={({ data }) => (
-                                                <div style={{ 
-                                                    color: 'white',
-                                                    fontWeight: 'bold',
-                                                    }}><br />
-                                                    <div style={{ fontSize: '14px' }}>{data.title}</div>
-                                                    <div style={{ color: '#55596F' }}>{data.hallName}</div>
-                                                    <div style={{ color: '#3E4152' }}>{data.lecturerName}</div>
-                                                </div>
-                                            )}
-                                        />
-                                        <Resources
-                                            data={resources}
-                                            mainResourceName="priorityId"
-                                        />
-                                        <IntegratedGrouping />
-                                        <IntegratedEditing />
+                            <WeekView
+                                startDayHour={8}
+                                endDayHour={18}
+                                excludedDays={excludedDays}
+                                intervalCount={1} // Display one day in WeekView
+                            //Display only Saturday or next week Sunday
+                            // startDay={saturdaySelected ? 0 : 1} // Display Saturday or Sunday
+                            // endDay={saturdaySelected ? 6 : 0} // Display Saturday or Sunday
+                            />
 
-                                        <AppointmentTooltip />
-                                        <AppointmentForm />
+                            <Appointments
+                                appointmentContentComponent={({ data }) => (
+                                    <div style={{
+                                        color: 'white',
+                                        fontWeight: 'bold',
+                                    }}><br />
+                                        <div style={{ fontSize: '14px' }}>{data.title}</div>
+                                        <div style={{ color: '#55596F' }}>{data.hallName}</div>
+                                        <div style={{ color: '#3E4152' }}>{data.lecturerName}</div>
+                                    </div>
+                                )}
+                            />
+                            <Resources
+                                data={resources}
+                                mainResourceName="priorityId"
+                            />
+                            <IntegratedGrouping />
+                            <IntegratedEditing />
 
-                                        <GroupingPanel />
-                                        <DragDropProvider />
-                                    </Scheduler>
-                              
-                        </TableContainer>
-                    </Paper>
-                </Box>
-            )
+                            <AppointmentTooltip />
+                            <AppointmentForm />
+
+                            <GroupingPanel />
+                            <DragDropProvider />
+                        </Scheduler>
+
+                    </TableContainer>
+                </Paper>
+            </Box>
+        )
     }
 }
